1a3e021611b01ff13266c350d1366c1e37cefaea
Bilawal Liaqat <bilawal.liaqat@gmail.com>
2021-07-29 16:56:04 +0500

    backend using starpi, react frontend using APIs

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e7cc510cf9c65b454e5533b84315b2c2'),
+    },
+  },
+});
